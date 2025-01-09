e8f7275a02b7e6e0f3be851059f736bd14bf9c9d
lielc3 <liel.marx-callaf@mail.huji.ac.il>
2025-01-09 02:57:03 +0200

    end of the day commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    // private InputSystem _moveAction;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
