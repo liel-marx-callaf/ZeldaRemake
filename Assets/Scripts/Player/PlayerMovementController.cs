9a1f099b62dcdd89bc3ff3cdb104f1ea72697e6f
lielc3 <liel.marx-callaf@mail.huji.ac.il>
2025-01-12 02:04:40 +0200

    added cameras and area transition

---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,105 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// todo: 4) public pushback function
+// todo: 6) can change collider size depending on direction
+
+public class PlayerMovementController : MonoBehaviour
+{
+    private static readonly int MoveHorizontal = Animator.StringToHash("MoveHorizontal");
+    private static readonly int MoveUp = Animator.StringToHash("MoveUp");
+    private static readonly int MoveDown = Animator.StringToHash("MoveDown");
+    [SerializeField] private float speed = 5f;
+
+    private Rigidbody2D _rb;
+    private InputPlayerActions _inputPlayerActions;
+    private InputAction _moveAction;
+    private Vector2 _moveDirection;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    
+    private void Awake()
+    {
+        _inputPlayerActions = new InputPlayerActions();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+    }
+    
+    private void OnEnable()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        
+        _moveAction = _inputPlayerActions.Player.Move;
+        _moveAction.Enable();
+        _moveAction.performed += OnMovePerformed;
+        _moveAction.canceled += OnMoveCanceled;
+    }
+
+
+    private void OnDisable()
+    {
+        _moveAction.performed -= OnMovePerformed;
+        _moveAction.canceled -= OnMoveCanceled;
+        _moveAction.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 moveInput = _moveAction.ReadValue<Vector2>();
+        PlayerMovement(moveInput);
+    }
+    
+    private void PlayerMovement(Vector2 moveInput)
+    {
+  
+        _rb.linearVelocity = _moveDirection * speed;
+
+    }
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        Vector2 moveInput = context.ReadValue<Vector2>();
+        
+        _animator.ResetTrigger(MoveHorizontal);
+        _animator.ResetTrigger(MoveUp);
+        _animator.ResetTrigger(MoveDown);
+        if (moveInput.y > 0)
+        {
+            _moveDirection = Vector2.up;
+            _spriteRenderer.flipX = false;
+            _animator.SetTrigger(MoveUp);
+            _animator.speed = 1;
+        }
+        else if (moveInput.y < 0)
+        {
+            _moveDirection = Vector2.down;
+            _spriteRenderer.flipX = false;
+            _animator.SetTrigger(MoveDown);
+            _animator.speed = 1;
+        }
+        else if (moveInput.x > 0)
+        {
+            _moveDirection = Vector2.right;
+            _spriteRenderer.flipX = false;
+            _animator.SetTrigger(MoveHorizontal);
+            _animator.speed = 1;
+        }
+        else if (moveInput.x < 0)
+        {
+            _moveDirection = Vector2.left;
+            _spriteRenderer.flipX = true;
+            _animator.SetTrigger(MoveHorizontal);
+            _animator.speed = 1;
+        }
+        else
+        {
+            _moveDirection = Vector2.zero;
+        }
+    }
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        // Vector2 moveInput = context.ReadValue<Vector2>();
+        _moveDirection = Vector2.zero;
+        _animator.speed = 0;
+    }
+}
