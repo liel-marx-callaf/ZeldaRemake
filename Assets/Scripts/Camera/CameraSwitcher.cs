e09689d41c09eea8bf8dd2bf6ffff9be2adabab2
lielc3 <liel.marx-callaf@mail.huji.ac.il>
2025-01-22 18:09:23 +0200

    changed the cameraSwitchers so that when transitioning between areas, cant trigger the area switch

--- Assets/Scripts/Camera/CameraSwitcher.cs
+++ Assets/Scripts/Camera/CameraSwitcher.cs
@@ -1,6 +1,8 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using Unity.Cinemachine;
-
+// todo: fix pixel perfect camera size and colliders
 public class CameraSwitcher : MonoBehaviour
 {
     [SerializeField] private CinemachineCamera currentCamera;
@@ -11,9 +13,38 @@ public class CameraSwitcher : MonoBehaviour
     // [SerializeField] private int currentIndex;
     // [SerializeField] private CinemachineCamera[] cameras;
     
+    // private Collider2D _collider;
+    private float _transitionDuration;
+    private bool _areaSwitching = false;
+
+    private void Start()
+    {
+        // _collider = GetComponent<Collider2D>();
+        _transitionDuration = Camera.main.GetComponent<CinemachineBrain>().DefaultBlend.Time;
+    }
+
+    private void OnEnable()
+    {
+        MyEvents.AreaSwitch += OnAreaSwitch;
+    }
+    
+    private void OnDisable()
+    {
+        MyEvents.AreaSwitch -= OnAreaSwitch;
+    }
+
+
+
+    private IEnumerator IgnoreTriggerTemporarily()
+    {
+        _areaSwitching = true;
+        yield return new WaitForSeconds(_transitionDuration);
+        _areaSwitching = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (_areaSwitching) return;
         if (other.CompareTag("Player"))
         {
             SwitchCamera();
@@ -31,5 +62,12 @@ public class CameraSwitcher : MonoBehaviour
         
         MyEvents.AreaSwitch?.Invoke(targetIndex, currentIndex);
     }
+        private void OnAreaSwitch(int enterIndex, int exitIndex)
+        {
+            if (enterIndex == currentIndex)
+            {
+                StartCoroutine(IgnoreTriggerTemporarily());
     
+            }
+        }
 }
