4f6b6aa7910ce4c3d60dc80a94e06f6e1415f500
lielc3 <liel.marx-callaf@mail.huji.ac.il>
2025-01-27 05:16:55 +0200

    started working on start screen, added triangle animation

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private Animator transition;
+    [SerializeField] private float transitionTime = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+        
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene("MainWorld");
+    }
+    
+    private IEnumerator LoadLevel(SceneIndexEnum sceneIndexEnum)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("MainWorld");
+    }
+}
