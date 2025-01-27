ad1df42d3521d48172bfbc2a56847a89798d5300
lielc3 <liel.marx-callaf@mail.huji.ac.il>
2025-01-27 06:19:09 +0200

    added Start screen, when press start button the main game loads. has background music

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -18,13 +18,13 @@ public class LevelLoader : MonoBehaviour
 
     private void LoadNextLevel()
     {
-        SceneManager.LoadScene("MainWorld");
+        StartCoroutine(LoadLevel(SceneIndexEnum.MainGame));
     }
     
     private IEnumerator LoadLevel(SceneIndexEnum sceneIndexEnum)
     {
         transition.SetTrigger("Start");
         yield return new WaitForSeconds(1);
-        SceneManager.LoadScene("MainWorld");
+        SceneManager.LoadScene("MainGame");
     }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,26 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 namespace Managers
 {
     public class GameManager : MonoSingleton<GameManager>
     {
+        private static readonly int Start1 = Animator.StringToHash("Start");
+
         [Header("Journal References")] [SerializeField]
         private GameObject journalCanvas;
 
         [SerializeField] private JournalController journalController;
         private bool _isJournalOpen;
 
-        [Header("Scene References")] [SerializeField]
-        private SceneIndexEnum startingScene;
+        [Header("Transition Settings")]
+        [SerializeField] private Animator transition;
+        [SerializeField] private float transitionTime = 1f;
+        
+        [Header("Scene References")] 
+        [SerializeField] private SceneIndexEnum startingScene;
         private SceneIndexEnum _currentScene;
 
         [Header("Area References")] [SerializeField]
@@ -22,6 +30,7 @@ namespace Managers
 
         private InputPlayerActions _inputPlayerActions;
         private InputAction _actionSelect;
+        private InputAction _actionStart;
 
         private void OnEnable()
         {
@@ -29,12 +38,17 @@ namespace Managers
             _actionSelect = _inputPlayerActions.Player.ActionSelect;
             _actionSelect.Enable();
             _actionSelect.performed += OnActionSelect;
+            _actionStart = _inputPlayerActions.Player.ActionStart;
+            _actionStart.Enable();
+            _actionStart.performed += OnActionStart;
         }
 
         private void OnDisable()
         {
             _actionSelect.performed -= OnActionSelect;
             _actionSelect.Disable();
+            _actionStart.performed -= OnActionStart;
+            _actionStart.Disable();
         }
 
         private void Start()
@@ -51,7 +65,26 @@ namespace Managers
                 ToggleJournal();
             }
         }
-
+        
+        private void OnActionStart(InputAction.CallbackContext context)
+        {
+            if (_currentScene is SceneIndexEnum.StartMenu)
+            {
+                LoadNextScene(SceneIndexEnum.MainGame);
+            }
+        }
+        private void LoadNextScene(SceneIndexEnum sceneIndexEnum)
+        {
+            StartCoroutine(LoadScene(sceneIndexEnum));
+        }
+    
+        private IEnumerator LoadScene(SceneIndexEnum sceneIndexEnum)
+        {
+            transition.SetTrigger(Start1);
+            yield return new WaitForSeconds(1);
+            SceneManager.LoadScene(sceneIndexEnum.ToString());
+            MyEvents.LoadScene?.Invoke(sceneIndexEnum);
+        }
 
         private void OpenJournal()
         {
